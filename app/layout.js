09a595d71a8d455daa8e11f28a3d49673ad68477
LordMarZ <marcelozaha@hotmail.com>
2026-05-22 17:35:53 -0300

    favicon balon

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,8 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Rajdhani:wght@400;500;600;700&display=swap" rel="stylesheet" />
-      </head>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>⚽</text></svg>" />
+    </head>
       <body>{children}</body>
     </html>
   )
