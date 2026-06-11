e5e860ef7af88fda9688f0f9d92c8843fbe02fdd
LordMarZ <marcelozaha@hotmail.com>
2026-06-11 11:22:59 -0300

    flag-icons local

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,6 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Rajdhani:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>⚽</text></svg>" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flag-icons@6.6.6/css/flag-icons.min.css" />
       </head>
       <body>{children}</body>
     </html>
